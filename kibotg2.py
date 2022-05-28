459891b25008686f25622383f0f1cda15884d46c
Kirill Morozov <ki.morozov@gmail.com>
2022-05-28 23:21:53 +0300

    kibotg2.1

--- kibotg2.py
+++ kibotg2.py
@@ -1,57 +1,51 @@
 import requests
+import time
 from datetime import datetime, date
 
-# Стандатные переменные
+def processing(chat_id,message,API_URL,d1,d2):
+    # Функция обработчик ответов
+    requests.get(f"{API_URL}/sendMessage?chat_id={chat_id}&text={message}")
+ #   print(f"Receive text: {message}, send message: {message}:\n")   
+    with open(f"LOG{d2}.log", "a", encoding='utf-8') as f:
+            f.write(f"{d1} Receive text: {message}, send message: {message}\n")
 
-TOKEN = "5309976823:AAE8RglgFdx2kOFYrmcYcZOfY1X2JRXFizQ"
-API_URL = "https://api.telegram.org"
-dt_now = datetime.now()
-d_now = date.today()
+def main():
+    
+    TOKEN = "5309976823:AAE8RglgFdx2kOFYrmcYcZOfY1X2JRXFizQ"
+    API_URL = f"https://api.telegram.org/bot{TOKEN}"
+    
+       
+    last_update = 0
 
-
-
-
-
-# Определение последней версии обновления
-with open("offset", "r", encoding='utf-8') as f:
-    LAST_UPDATE = int(f.read())
-
-d1 = d_now.strftime("%Y%m%d")
-d2 = dt_now.strftime("%d/%m/%Y %H:%M:%S")
-request_url = f"{API_URL}/bot{TOKEN}/getUpdates?offset={LAST_UPDATE+1}"
-
-with open(f"logs/{d1}.log", "a", encoding='utf-8') as f:
-    f.write(f"{d2} START, Last update: {LAST_UPDATE}, Request URL: {request_url}, Responce: {requests.get(request_url)} \n")
-
-# START обработки
-
-if str(requests.get(request_url)) != "<Response [200]>":
-    with open(f"logs/{d1}.log", "a", encoding='utf-8') as f:
-        f.write(f"{d2} STOP - Request Errore \n")
-else:
-    with open(f"logs/{d1}.log", "a", encoding='utf-8') as f:
-        f.write(f"{d2} run processing \n")
+    while True:
+        time.sleep(5)
+        dt_now = datetime.now()
+        d_now = date.today()
+        d1 = dt_now.strftime("%d/%m/%Y %H:%M:%S")
+        d2 = d_now.strftime("%Y%m%d")
+        request_url = f"{API_URL}/getUpdates?offset={last_update+1}"
+              
+#        print(f"{d1} REQUEST Last update: Request URL: {request_url}, Responce: {requests.get(request_url)}")
+        with open(f"LOG{d2}.log", "a", encoding='utf-8') as f:
+            f.write(f"{d1} REQUEST URL: {request_url}, Responce: {requests.get(request_url)} \n")
 
         updates = requests.get(request_url).json()
 
-    if len(updates["result"]) == 0:
-        with open(f"logs/{d1}.log", "a", encoding='utf-8') as f:
-            f.write(f"{d2} STOP - No updates. \n")
-    else:
-        updates = updates["result"]
-        for update in updates:
-            update_id = update["update_id"]
-            chat_id = update["message"]["chat"]["id"]
-            message = update["message"]["text"]
+        if len(updates["result"]) == 0:
+            with open(f"LOG{d2}.log", "a", encoding='utf-8') as f:
+                f.write(f"{d1} No updates \n")
+     #       print(f"{d1} No updates \n")
+            continue
+        else:
+            updates = updates["result"]
+            for update in updates:
+                update_id = update["update_id"]
+                chat_id = update["message"]["chat"]["id"]
+                message = update["message"]["text"]
 
-            requests.get(f"{API_URL}/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}")
+                processing(chat_id,message,API_URL,d1,d2)
+                
+                last_update = update_id
 
-            with open(f"logs/{d1}.log", "a", encoding='utf-8') as f:
-                f.write(f"{d2} receive text:{message}, send message{message}:\n")
-
-            with open("offset", "w") as f:
-                f.write(str(update_id))
-
-        with open(f"logs/{d1}.log", "a", encoding='utf-8') as f:
-            f.write(f"{d2} STOP\n")
-
+if __name__ == "__main__":
+    main()
